a60ace20c9be0e903e429274bbb42f323b00da71
Geiselle Holt (Ada Developers Academy) <v-gholt@microsoft.com>
2023-03-10 15:24:49 -0800

    Layout for Home Page

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -24,5 +24,15 @@ namespace WPFThreeDogsDistillery2
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
